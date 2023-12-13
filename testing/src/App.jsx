f40d81c9b90159e91b00de653b8b530c795f2d3d
sanashish <imsanashish@gmail.com>
2023-12-13 10:55:58 +0530

    ConditionalRendering

--- testing/src/App.jsx
+++ testing/src/App.jsx
@@ -1,12 +1,23 @@
 import "./App.css";
 
 function App() {
-  let city = ["Lucknow", "Pune", "Banglore", "Kolkata"];
+  // let city = ["Lucknow", "Pune", "Banglore", "Kolkata"];
+  let city = [];
+
+  // if (city.length === 0) {
+  //   return <h4>No City Found!</h4>;
+  // }
+
+  let msg = city.length === 0 ? <h4>No City Found!</h4> : null;
+
   return (
     <>
       <h1>
         <center>City</center>
       </h1>
+      {/* {city.length === 0 ? <h4>No City Found!</h4>:null} */}
+      {/* {msg} */}
+      {city.length === 0 && <h4>No City Found!</h4>}
       <ul className="list-group mx-5">
         {/* ------------------Map Method---------------- */}
         {/* ---------Render Lists from array data------- */}
