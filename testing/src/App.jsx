9de99a74595e4c482fac9fad8c9702cdb1a23675
sanashish <imsanashish@gmail.com>
2023-12-20 17:55:15 +0530

    Passing Children

--- testing/src/App.jsx
+++ testing/src/App.jsx
@@ -1,17 +1,25 @@
 import ErrorMsg from "./components/ErroMsg";
 import CityList from "./components/CityList";
 import "./App.css";
+import Container from "./components/Container";
 
 function App() {
   let city = ["Lucknow", "Pune", "Banglore", "Kolkata"];
 
   return (
     <>
-      <h1 className="title">
-        <center>City</center>
-      </h1>
-      <ErrorMsg city={city} />
-      <CityList city={city} />
+      <Container>
+        <h1 className="title">
+          <center>City</center>
+        </h1>
+        <ErrorMsg city={city} />
+        <CityList city={city} />
+      </Container>
+      <Container>
+        <p>
+          <center>These are some cities name</center>
+        </p>
+      </Container>
     </>
   );
 }
