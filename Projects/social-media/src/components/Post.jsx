0eee273e5ea3a886a275cc0cc91078ba498f741e
sanashish <imsanashish@gmail.com>
2024-01-19 00:48:44 +0530

    Social Media Basic UI

---
+++ Projects/social-media/src/components/Post.jsx
@@ -0,0 +1,17 @@
+function Post() {
+  return (
+    <div className="card w-50">
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Button
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
