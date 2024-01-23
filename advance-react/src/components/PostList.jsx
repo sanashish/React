d84d69f6299dcaf15d936396c231cbe9e62a0af1
sanashish <imsanashish@gmail.com>
2024-01-23 17:10:32 +0530

    Handling Loading State

--- advance-react/src/components/PostList.jsx
+++ advance-react/src/components/PostList.jsx
@@ -1,25 +1,29 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import Message from "./Message";
+import LoadingSpinner from "./LoadingSpinner";
 
 function PostList() {
   const { postList, addPosts } = useContext(PostListData);
 
+  const [fetching, setFetching] = useState(false);
+
   useEffect(() => {
+    setFetching(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addPosts(data.posts);
+        setFetching(false);
       });
   }, []);
 
   return (
     <>
-      {postList.length === 0 && <Message />}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <Message />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 }
