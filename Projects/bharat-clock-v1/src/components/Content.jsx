247bc0c823f2c8c78592ab99f4468688a9d4a16b
sanashish <imsanashish@gmail.com>
2024-01-23 19:02:19 +0530

    v1.1

--- Projects/bharat-clock-v1/src/components/Content.jsx
+++ Projects/bharat-clock-v1/src/components/Content.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
+
 function Content() {
-  let date = new Date();
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <>
       <div className="content">
         <div>This is the clock that shows the time in Bharat at all times</div>
         <div>
-          This is the current time: {date.toLocaleDateString("en-GB")} -{" "}
-          {date.toLocaleTimeString()}
+          This is the current time: {time.toLocaleDateString("en-GB")} -{" "}
+          {time.toLocaleTimeString()}
         </div>
       </div>
     </>
