7bf9195a4be9db85a7f159de9c9ade9eaba78c6a
sanashish <imsanashish@gmail.com>
2024-01-23 21:51:38 +0530

    useCallback Hook

--- advance-react/src/store/post-list-store.jsx
+++ advance-react/src/store/post-list-store.jsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { createContext, useReducer } from "react";
 
 export const PostList = createContext({
@@ -48,12 +49,15 @@ const PostListProvider = ({ children }) => {
     });
   };
 
-  const deletePost = (postId) => {
-    dispatchPostList({
-      type: "DELETE_POST",
-      payload: { postId },
-    });
-  };
+  const deletePost = useCallback(
+    (postId) => {
+      dispatchPostList({
+        type: "DELETE_POST",
+        payload: { postId },
+      });
+    },
+    [dispatchPostList]
+  );
 
   return (
     <PostList.Provider
