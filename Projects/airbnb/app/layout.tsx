d02780edb4910ab92dc5c0f723a2cc7cc480561f
sanashish <imsanashish@gmail.com>
2024-04-27 22:46:28 +0530

    Airbnb Setup

---
+++ Projects/airbnb/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={(inter.className, font.className)}>{children}</body>
+    </html>
+  );
+}
